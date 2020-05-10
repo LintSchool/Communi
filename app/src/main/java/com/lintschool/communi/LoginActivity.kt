74e0f9cc4e3dd083c4308cd7eeb308afb560e8fa
mosaab <mosabahmeddev@gmail.com>
2020-05-10 22:53:54 +0200

    adding Login & Signup Activities

---
+++ app/src/main/java/com/lintschool/communi/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.lintschool.communi
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
---
+++ app/src/main/java/com/lintschool/communi/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.lintschool.communi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
