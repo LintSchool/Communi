2806f5ea27425bc6e3179f246b127111168d89d5
Mohamed Hefny <hefny.7@gmail.com>
2020-05-18 21:25:48 +0200

    Add group chat screen.

---
+++ app/src/main/java/com/lintschool/communi/ChatGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.lintschool.communi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatGroupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_group)
+    }
+}
